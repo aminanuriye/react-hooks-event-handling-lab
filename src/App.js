1fd34265a2db1b437f515f898e05a312144a36d4
aminanuriye <aminaanuriye@gmail.com>
2025-08-01 14:07:42 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Keypad from './components/Keypad';
+import EyesOnMe from './components/EyesOnMe';
+
+function App() {
+  return (
+    <div>
+      <h1>Event Handling Lab</h1>
+      <Keypad />
+      <br /><br />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
